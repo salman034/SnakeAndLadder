9e5b50b56c3fc52ecb3f6cef0f0a96b554bb9e6f
unknown <you@example.com>
2022-03-13 16:44:46 +0530

    Added: Player rolls th die to get a number

--- src/snakeAndLadderProblem/snakeNLadder.java
+++ src/snakeAndLadderProblem/snakeNLadder.java
@@ -3,6 +3,10 @@ package snakeAndLadderProblem;
 public class snakeNLadder {
     public static void main(String[] args) {
         int player = 0;
+        int SNAKE = 0;
+        int LADDER = 0;
         System.out.println("Single Player at start position 0: " + player);
+        int DIES = (int)((Math.random() * 10) % 6) +1;
+        System.out.println("Number of Dies:" + DIES);
     }
 }
