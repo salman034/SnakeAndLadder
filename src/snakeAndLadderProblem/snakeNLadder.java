dce36ac90bf34bf2e5fe5b24aadb002f8a2a9b7a
unknown <you@example.com>
2022-03-13 18:01:38 +0530

    Added: Player checks for option

--- src/snakeAndLadderProblem/snakeNLadder.java
+++ src/snakeAndLadderProblem/snakeNLadder.java
@@ -2,11 +2,23 @@ package snakeAndLadderProblem;
 
 public class snakeNLadder {
     public static void main(String[] args) {
-        int player = 0;
-        int SNAKE = 0;
-        int LADDER = 0;
+        int player = 1; int SNAKE = 0; int LADDER = 2;
         System.out.println("Single Player at start position 0: " + player);
+
         int DIES = (int)((Math.random() * 10) % 6) +1;
         System.out.println("Number of Dies:" + DIES);
+
+        int check = (int)(Math.random() * 3 + 1);
+        if (check == SNAKE) {
+            player = player - DIES;
+            System.out.println("Player stays in the same position :" + player);
+        }
+            else if (check == LADDER) {
+            player = player + DIES;
+            System.out.println("Player Moves ahead :"+ player);
+        }
+        else
+            System.out.println("Player moves Behind");
     }
 }
+
